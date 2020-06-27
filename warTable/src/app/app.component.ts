66d1f6465e6f5d122b2028037d38b6e51f86c12c
DrFriendless <friendless.farrell@gmail.com>
2020-06-27 14:59:54 +1000

    update war table package versions, convert table to use column definitions and doc from JSON, add tooltip to CFM column

--- warTable/src/app/app.component.ts
+++ warTable/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnDestroy, ViewEncapsulation } from '@angular
 import { Subscription } from "rxjs/internal/Subscription";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { WarTableRow } from "extstats-core";
+import {Column, ColumnParams} from "extstats-datatable";
 
 @Component({
   selector: 'war-table',
@@ -10,14 +11,41 @@ import { WarTableRow } from "extstats-core";
 })
 export class WarTableComponent implements OnDestroy, AfterViewInit {
   private loadData$;
-  public rows: WarTableRow[] = [];
   private subscription: Subscription;
-  public docCollapsed = true;
-  public loading = false;
+  private params: ColumnParams<WarTableRow>[] = [
+    {field: "geekName", name: "Geek",
+      valueHtml: (row: WarTableRow) => `<a href="/geek.html?geek=${row.geekName}">${row.geekName}</a>` },
+    {field: "totalPlays", name: "Total Plays", classname: "narrow",
+      tooltip: "Total plays of all games as recorded by Extended Stats"},
+    {field: "distinctGames", name: "Distinct Games", classname: "narrow",
+      tooltip: "The number of times you have played this game"},
+    {field: "owned", name: "Owned", classname: "narrow", tooltip: "Number of games this geek owns"},
+    {field: "want", name: "Want in Trade", classname: "narrow", tooltip: "Number of games this geek wants in trade"},
+    {field: "wish", name: "Wishlist", classname: "narrow", tooltip: "Number of games on this geek's wishlist"},
+    {field: "trade", name: "For Trade", classname: "narrow", tooltip: "Number of games this geek has for trade" },
+    {field: "sdj", name: "SdJ", classname: "narrow",
+      tooltip: "Number of different Spiel des Jahre winners played"},
+    {field: "top50", name: "BGG Top 50", classname: "narrow",
+      tooltip: "Number of different games in the BGG Top 50 played"},
+    {field: "ext100", name: "Extstats Top 100", classname: "narrow",
+      tooltip: "Number of different games in the Extended Stats Top 100 played"},
+    {field: "prevOwned", name: "Prev Owned", classname: "narrow", tooltip: "Number of games previously owned by this geek"},
+    {field: "friendless", name: "Friendless Metric", classname: "narrow", tooltip: "Friendless Metric for this geek"},
+    {field: "cfm", name: "CFM", classname: "narrow", tooltip: "Continuous Friendless Metric for this geek",
+      valueTooltip: (row: WarTableRow) => `Utilisation ${row.utilisation}%`},
+    {field: "zeros", name: "0s", classname: "narrow", tooltip: "Number of games this geek owns that they have played 0 times"},
+    {field: "tens", name: "10s", classname: "narrow", tooltip: "Number of games this geek owns that they have played 10+ times"},
+    {field: "hindex", name: "H-Index", classname: "narrow", tooltip: "This geek's H-index"},
+    {field: "hrindex", name: "Hr-Index", classname: "narrow", tooltip: "This geek's rational H-index"},
+    {field: "gindex", name: "G-Index", classname: "narrow", tooltip: "This geek's G-index"},
+  ];
+  columns = this.params.map(c => new Column<WarTableRow>(c));
+  rows: WarTableRow[] = [];
+  loading = false;
 
   constructor(private http: HttpClient) {}
 
-  public ngAfterViewInit(): void {
+  ngAfterViewInit(): void {
     const options = {
       headers: new HttpHeaders().set("x-api-key", "gb0l7zXSq47Aks7YHnGeEafZbIzgmGBv5FouoRjJ")
     };
@@ -33,7 +61,7 @@ export class WarTableComponent implements OnDestroy, AfterViewInit {
     });
   }
 
-  public ngOnDestroy(): void {
+  ngOnDestroy(): void {
     if (this.subscription) this.subscription.unsubscribe();
   }
 }
--- warTable/src/app/app.module.ts
+++ warTable/src/app/app.module.ts
@@ -3,17 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { WarTableComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
-import { DataTableModule } from "extstats-datatable";
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ExtstatsAngularModule } from "extstats-angular"
+import {ExtstatsDatatableModule} from "extstats-datatable";
 
 @NgModule({
   declarations: [
     WarTableComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, DataTableModule, TooltipModule.forRoot(), NgbModule.forRoot(), ExtstatsAngularModule
+    BrowserModule, HttpClientModule, ExtstatsDatatableModule, ExtstatsAngularModule
   ],
   providers: [],
   bootstrap: [WarTableComponent]
