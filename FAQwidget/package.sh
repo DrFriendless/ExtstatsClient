9a4d9a9766a73c24d792fb89448618c322afa7da
john <john@iwannaticket.com.au>
2025-10-21 14:00:27 +1100

    fix build for new Angular

--- FAQwidget/package.sh
+++ FAQwidget/package.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+rm ./dist/browser/*.js.map
 npm run build
-cp ./dist/*.js ../../extstats-home/public/FAQWidget/
+cp ./dist/browser/*.js ../../extstats-home/public/FAQWidget/
+cp ./dist/browser/*.js.map ../../extstats-home/public/FAQWidget/
 cp ./src/styles.css ../../extstats-home/public/FAQWidget/
 echo Files were copied to extstats-home.
