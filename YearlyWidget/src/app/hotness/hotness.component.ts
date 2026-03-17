9112336264bbd52bb833ced838080c4c42e586e9
john <john@iwannaticket.com.au>
2026-03-18 08:51:31 +1100

    update Yearly Plays with new column width stuff

--- YearlyWidget/src/app/hotness/hotness.component.ts
+++ YearlyWidget/src/app/hotness/hotness.component.ts
@@ -31,31 +31,31 @@ export class YearlyHotnessComponent implements OnChanges {
       name: "Game",
       tooltip: "The hot game",
       valueHtml: (r: MostPlayedEntry) =>  `<a href="https://boardgamegeek.com/boardgame/${r.bggid}">${r.name}</a>`,
-      classname: "wide"
+      classname: "col-game-name"
     },
-    { field: "geeks", name: "Geeks Playing", tooltip: "Distinct players of the game in this year" },
-    { field: "plays", name: "Total Plays", tooltip: "Plays by all players in this year" },
-    { field: "rating", name: "Rating", tooltip: "Your rating for this game",
+    { field: "geeks", name: "Geeks Playing", tooltip: "Distinct players of the game in this year", classname: "col-number" },
+    { field: "plays", name: "Total Plays", tooltip: "Plays by all players in this year", classname: "col-number" },
+    { field: "rating", name: "Rating", tooltip: "Your rating for this game", classname: "col-rating",
       valueHtml: (r: MostPlayedEntry) => (r.rating && r.rating > 0) ? r.rating.toString() : ""
     },
     {
-      field: "wantToBuy", name: "WTB", tooltip: "Want to buy",
+      field: "wantToBuy", name: "WTB", tooltip: "Want to buy", classname: "col-boolean",
       valueHtml: (r: MostPlayedEntry) => r.wantToBuy ? "✓" : ""
     },
     {
-      field: "wantToPlay", name: "WTP", tooltip: "Want to play",
+      field: "wantToPlay", name: "WTP", tooltip: "Want to play", classname: "col-boolean",
       valueHtml: (r: MostPlayedEntry) => r.wantToPlay ? "✓" : ""
     },
     {
-      field: "wantInTrade", name: "WIT", tooltip: "Want in trade",
+      field: "wantInTrade", name: "WIT", tooltip: "Want in trade", classname: "col-boolean",
       valueHtml: (r: MostPlayedEntry) => r.wantInTrade ? "✓" : ""
     },
     {
-      field: "wish", name: "Wish", tooltip: "Your wishlist rating for this game",
+      field: "wish", name: "Wish", tooltip: "Your wishlist rating for this game", classname: "col-number",
       valueHtml: (r: MostPlayedEntry) => r.wish > 0 ? r.wish.toString() : ""
     },
     {
-      field: "plays", name: "Your Plays", tooltip: "Your plays of this game",
+      field: "plays", name: "Your Plays", tooltip: "Your plays of this game", classname: "col-number",
       valueHtml: (r: MostPlayedEntry) => r.yourPlays > 0 ? r.yourPlays.toString() : ""
     }
   ];
