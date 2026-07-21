d681b2e157c2821a6b06f99e2ca484556f681e41
john <john@iwannaticket.com.au>
2026-07-21 13:43:16 +1000

    update versions of extstats packages

--- PlaysWidget/deploy.sh
+++ PlaysWidget/deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-cp ./dist/browser/*.js ../../extstats-home/public/PlaysWidget/
-cp ./dist/browser/*.js.map ../../extstats-home/public/PlaysWidget/
-cp ./src/styles.css ../../extstats-home/public/PlaysWidget/
-echo Files were copied to extstats-home.
+COMPONENT=PlaysWidget
+DEST=../../extstats-home/public/$COMPONENT
+mkdir -p $DEST
+cp ./dist/browser/*.js $DEST
+cp ./dist/browser/*.js.map $DEST
+cp ./src/styles.css $DEST
+echo Files were copied to $DEST
